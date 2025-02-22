de6292babb54860ed129e864b76b26e8b5053580
jabalamelian <z.jabalamelian@gmail.com>
2025-02-22 22:47:40 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,8 @@ import telebot
 TOKEN ="8058278858:AAHLcScvnAdkjovjPyo9xK8q3mN956siAd8"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "سلام! من ربات تلگرامی شما هستم.")
 
-bot.polling()
+bot.polling(none_stop=True)  # این خط فقط وقتی باید اجرا بشه که Webhook فعال نباشه
